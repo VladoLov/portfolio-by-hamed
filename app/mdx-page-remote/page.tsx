7f760275dbe79cb8b1f487159ef3f5c4aa8b31fa
VladoLov <vladislav.lovric@gmail.com>
2024-10-16 15:27:48 +0200

    update typscirpt errors

--- app/mdx-page-remote/page.tsx
+++ app/mdx-page-remote/page.tsx
@@ -1,8 +1,18 @@
 import { MDXRemote } from 'next-mdx-remote/rsc'
 
 export default async function RemoteMdxPage() {
-  // MDX text - can be from a local file, database, CMS, fetch, anywhere...
-  const res = await fetch('https://...')
-  const markdown = await res.text()
-  return <MDXRemote source={markdown} />
+  try {
+    // Replace with the actual URL
+    const res = await fetch('https://portfolio-by-hamed.vercel.app/')
+
+    if (!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`)
+    }
+
+    const markdown = await res.text()
+    return <MDXRemote source={markdown} />
+  } catch (error) {
+    console.error('Error fetching MDX:', error)
+    return <div>Error loading content: {error.message}</div> // Display an error message to the user
+  }
 }
