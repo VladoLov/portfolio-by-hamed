435dff62b543d62b8170aaa78ae62d6bada0b7c3
VladoLov <vladislav.lovric@gmail.com>
2024-10-24 11:58:01 +0200

    add new posts

--- components/header.tsx
+++ components/header.tsx
@@ -5,9 +5,9 @@ import Link from 'next/link'
 export default function Header() {
   return (
     <header className='fixed inset-x-0 top-0 z-50 bg-background/75 py-6'>
-      <nav className='container flex max-w-3xl items-center justify-between'>
+      <nav className='mx-auto flex w-max max-w-3xl items-center justify-between px-1 md:container'>
         <div>
-          <Link href='/' className='font-serif text-2xl font-bold'>
+          <Link href='/' className='pr-6 font-serif text-2xl font-bold sm:pr-0'>
             NAXX
           </Link>
         </div>
